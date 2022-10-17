eef396a2090f6eaca13efbd4da30eeac87b179cc
tesfahun <y.w.tesfahun@gmail.com>
2022-10-17 15:15:40 +0100

    registration util test added

---
+++ app/src/test/java/com/tes/eat/anywhere/shopping/RegistrationUtilTest.kt
@@ -0,0 +1,22 @@
+package com.tes.eat.anywhere.shopping
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class RegistrationUtilTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun validateRegistrationInput() {
+    }
+}
