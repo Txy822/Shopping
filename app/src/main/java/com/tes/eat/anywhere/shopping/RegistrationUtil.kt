2a080072b63237d6ef5a63f97a676d2d69a75d58
tesfahun <y.w.tesfahun@gmail.com>
2022-10-17 15:14:02 +0100

    Initial commit

---
+++ app/src/main/java/com/tes/eat/anywhere/shopping/RegistrationUtil.kt
@@ -0,0 +1,17 @@
+package com.tes.eat.anywhere.shopping
+
+object RegistrationUtil {
+/*
+the input is not valid if pass/username is empty and
+the user name is taken
+the confirmed password is not the same
+password contains less than 2 char
+ */
+    fun validateRegistrationInput(
+        username:String,
+        password:String,
+        ConfirmedPassword:String
+    ):Boolean {
+        return true
+    }
+}
